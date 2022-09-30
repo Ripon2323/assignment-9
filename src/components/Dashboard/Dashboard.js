39706fe7ecbe440067b6edd9a01e8c3a3af25415
Ripon2323 <ripon23.hawladar@gmail.com>
2022-10-01 01:48:50 +0600

    bar-chart added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,8 @@
 
+
 import React from 'react';
-import { CartesianGrid, Legend, Line, LineChart, XAxis, YAxis } from 'recharts';
+import {  CartesianGrid, Legend, Line, LineChart, Pie, PieChart,  XAxis, YAxis } from 'recharts';
+import './Dashboard.css';
 
 const Dashboard = () => {
 
@@ -44,18 +46,31 @@ const Dashboard = () => {
     ]
 
     return (
-        <div>
+        <div className='line-chart'>
 
-            <LineChart width={600} height={200} data={data}
-                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="month" />
-                <YAxis />
-                
-                <Legend />
-                <Line type="monotone" dataKey="sell" stroke="#8884d8" />
-                <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
-            </LineChart>
+            <div>
+                <h3 className='line-chart-color'>Month wise sell</h3>
+                <LineChart width={600} height={200} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="month" />
+                    <YAxis />
+
+                    <Legend />
+                    <Line type="monotone" dataKey="sell" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="revenue" stroke="#82ca9d" />
+                </LineChart>
+            </div>
+
+            <div>
+                <h3 className='line-chart-color'>Investment VS Revenue</h3>
+                <PieChart width={730} height={250}>
+                    <Pie data={data} dataKey="investment" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="revenue" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                </PieChart>
+            </div>
+            
+
         </div>
     );
 };
