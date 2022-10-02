0f9b7c4ac8ae598b84d7d3e0db8a3459f204adf7
Ripon2323 <ripon23.hawladar@gmail.com>
2022-10-02 14:05:56 +0600

    readme file edited

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import './Blog.css'
 
 
 
 const Blog = () => {
-   
+
     return (
         <div>
-       
-            
+            <div>
+                <h1 className='blog-color'>What is context api? </h1>
+                <p className='blog-paragraph'>Context api is an important topic in react js.It provides a way to pass
+                    data through the components without passing props.It is quite different.
+                    Normally data is transferred one component to another component through props.It
+                    is primarily used when some data needs to be accessible by many components at different
+                    nesting levels.If anyone want to avoid passing some props through many levels,components composition is
+                    often a simpler solution than context.React.createContext() is all we need.It returns a consumer and provider.
+                    Provider is a component that as its suggest the state to its children.It will hold the store and be the parent
+                    of all the component that might need that store.So these are the main feature of context api.
+                </p>
+            </div>
+            <div>
+                <h1 className='blog-color'>What is Semantic Tag</h1>
+                <p className='blog-paragraph'>In programming semantic means the piece of a code.It is used in html, javascript and many other
+                    languages.HTML was originally created as a markup language to describe documents on the early internet.As the internet grew was adopted by more
+                    people,it needs changed.Where the internet was originially intended for sharing documents,now people wants to new things.
+                    People started to look their website look nicer.Initially programmer can't use may semantic element thats why the website didn't
+                    look more nice.They didn't use class ,id to design the website.HTML5 has some semantic element.They are
+                    header,nav,article,time,aside.Before creating semantic element programmer used it different ways.So,these are the basic things about
+                    semantic element. 
+                </p>
+
+            </div>
+
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,42 +14,42 @@ const Home = () => {
             "id": 1,
             "name": "Rakib",
             "ratings": 4,
-            "img": "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/fbaf991a78bc4896a3e9ad7800abcec6_9366/Ultraboost_22_Shoes_Black_GZ0127_01_standard.jpg",
+            "img":"https://img.freepik.com/premium-photo/young-handsome-man-with-beard-isolated-keeping-arms-crossed-frontal-position_1368-132662.jpg?w=2000",
             "comment":"The product is very nice and giving me better experience"
         },
         {
             "id": 2,
             "name": "Salman",
             "ratings": 4.2,
-            "img": "https://images.app.goo.gl/7QeeJGk2Njx55D5J7",
+            "img": "https://media.istockphoto.com/photos/shot-of-a-handsome-young-man-standing-against-a-grey-background-picture-id1335941248?b=1&k=20&m=1335941248&s=170667a&w=0&h=sn_An6VRQBtK3BuHnG1w9UmhTzwTqM3xLnKcqLW-mzw=",
             "comment":"Yeahh!!! the laptop is very cool"
         },
         {
             "id": 3,
             "name": "Niloy",
             "ratings": 4.4,
-            "img": "https://images.app.goo.gl/jwaZLttSjB6ZyNJx9",
+            "img": "https://media.istockphoto.com/photos/confident-businessman-posing-in-the-office-picture-id891418990?b=1&k=20&m=891418990&s=612x612&w=0&h=b03BA31GLFG5PTaDG1EGcxEPYfnH7ruCaJpBtA9JtxU=",
             "comment":"This is very happy thing to say that the product was very helful for me"
         },
         {
             "id": 4,
             "name": "Akib",
             "ratings": 4.6,
-            "img": "https://images.app.goo.gl/jradA3wyf3u54FqJ6",
+            "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsck-nnNp0FRN4JjPujQH95zYSDhmYnRWIVh72b9HOvtjvSV3ou3cuhBElr9w3QyjKCvo&usqp=CAU",
             "comment":"This is very happy thing to say that the product was very helful for me"
         },
         {
             "id": 5,
             "name": "Akash",
             "ratings": 4.8,
-            "img": "https://images.app.goo.gl/vh5cJCS4eBRzk9xY7",
+            "img": "https://media.istockphoto.com/photos/shot-of-a-handsome-young-businessman-standing-alone-in-his-office-picture-id1338606557?b=1&k=20&m=1338606557&s=170667a&w=0&h=jU64Sa-G06KGDUNoGJWcxd8G0udFOE_mtYM1I5f7zas=",
             "comment":"This is very happy thing to say that the product was very helful for me"
         },
         {
             "id": 6,
             "name": "Nila",
             "ratings": 5,
-            "img": "https://images.app.goo.gl/mrAC5YNKQn",
+            "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpYy7jWHtEiERlPEkMaikKcWlY-froYQdnrQ&usqp=CAU",
             "comment":"This is very happy thing to say that the product was very helful for me"
         }
     ]
